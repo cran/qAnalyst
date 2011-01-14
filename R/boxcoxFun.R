a709e3bc04776e4ade34db89e9df17f780ff85fb
Andrea Spano <andrea.spano@quantide.com>
2011-01-14 00:00:00 +0000

    version 0.6.4

--- R/boxcoxFun.R
+++ R/boxcoxFun.R
@@ -1,14 +1,17 @@
-boxcoxFun<-function(x)
-{
-	#check if all values are positive
-	checkPositivity=ifelse(x>0,TRUE,FALSE)
-	if (!all(checkPositivity)) stop("Error! All x must be positive") 
-	#creates bc obje by bctrans1 of alr3 package
-	bcObj=bctrans1(X=x,family="box.cox")
-	lambda=coef(bcObj)
-	y=powtran(bcObj, lambda=coef(bcObj), family=bcObj$family,modified=FALSE)
-	y=y[!is.na(y)]
-	outList=list(type="boxcox", parameters=lambda, original=x,transformed=as.numeric(y))
-	class(outList)="transformation"
-	invisible(outList)
+boxcoxFun = function(x) {
+  # Check if all values are positive
+  checkPositivity = ifelse(x > 0, TRUE, FALSE)
+  if (!all(checkPositivity)) stop("Error! All x must be positive")
+
+
+  # Creates bc object by powerTransform of car package
+  bcObj = powerTransform(x)
+  lambda = coef(bcObj)
+
+  y = bcPower(x, lambda = coef(bcObj))
+  y = y[!is.na(y)]
+
+  outList = list(type = "boxcox", parameters = lambda, original = x, transformed = as.numeric(y))
+  class(outList) = "transformation"
+  invisible(outList)
 }
